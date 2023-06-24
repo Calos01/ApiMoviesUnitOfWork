5e1ace019423dda5d5038b12420342c3d5d8720f
Calos01 <ca_los01@hotmail.com>
2023-06-24 17:27:46 -0500

    estructura generica

---
+++ ApiMoviesUnitOfWork/MoviesManagement.DataAccess/Implementacion/MovieRepository.cs
@@ -0,0 +1,18 @@
+﻿using MoviesManagement.DataAccess.Context;
+using MoviesManagement.Domain.Entities;
+using MoviesManagement.Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesManagement.DataAccess.Implementacion
+{
+    internal class MovieRepository : GenericRepository<Movie>, IMovieRepository
+    {
+        public MovieRepository(MoviesManagementDbContext context) : base(context)
+        {
+        }
+    }
+}
