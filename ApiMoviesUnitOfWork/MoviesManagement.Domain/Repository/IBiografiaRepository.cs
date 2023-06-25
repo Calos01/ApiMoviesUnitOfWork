7e257fbeb19a2bfb839d1dfb8172a1c91b4c68dd
Calos01 <ca_los01@hotmail.com>
2023-06-24 19:56:21 -0500

    estructura generica

--- ApiMoviesUnitOfWork/MoviesManagement.Domain/Repository/IBiografiaRepository.cs
+++ ApiMoviesUnitOfWork/MoviesManagement.Domain/Repository/IBiografiaRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MoviesManagement.Domain.Repository
 {
-    internal interface IBiografiaRepository
+    public interface IBiografiaRepository
     {
     }
 }
